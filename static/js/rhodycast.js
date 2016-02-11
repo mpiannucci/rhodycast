01f5d2cafecc6386eae476469a8f5e84008a2b40
Matthew Iannucci <miannucci@navatekltd.com>
2016-02-11 16:11:00 -0500

    Beautiful chart

--- static/js/rhodycast.js
+++ static/js/rhodycast.js
@@ -17,44 +17,56 @@ function initMap() {
 
 $(document).ready(function() {
     // Setup the chart!
-    var chart = new CanvasJS.Chart('waveHeightChart', {
-        title: {
-            text: "Wave Height (ft)",
-            verticalAlign: "top", // "top", "center", "bottom"
-            horizontalAlign: "center", // "left", "right", "center"
-            fontSize: 15,
-            margin: -15,
-            fontWeight: "bolder"
-        },
-        axisY: {
-            gridColor: "white",
-            lineColor: "#F2F2F2",
-            tickColor: "#F2F2F2",
-            gridThickness: 0           
-        },
-        axisX: {
-            lineColor: "#F2F2F2",
-            tickColor: "#F2F2F2"
-        },
-        data: [
-        {        
-            type: "spline", dataPoints: [
-            { x: new Date(2012, 00, 1), y: 1352 },
-            { x: new Date(2012, 01, 1), y: 1514 },
-            { x: new Date(2012, 02, 1), y: 1321 },
-            { x: new Date(2012, 03, 1), y: 1163 },
-            { x: new Date(2012, 04, 1), y: 950 },
-            { x: new Date(2012, 05, 1), y: 1201 },
-            { x: new Date(2012, 06, 1), y: 1186 },
-            { x: new Date(2012, 07, 1), y: 1281 },
-            { x: new Date(2012, 08, 1), y: 1438 },
-            { x: new Date(2012, 09, 1), y: 1305 },
-            { x: new Date(2012, 10, 1), y: 1480 },
-            { x: new Date(2012, 11, 1), y: 1291 }        
-            ]
-        }       
-        ]
-    });
+    $.ajax({
+        url: '/forecast_as_json',
+        type: 'GET'
+    }).done(function(data) {
 
-    chart.render();
+        dataSeries = [];
+        for (var i = 0; i < data.ForecastData.length; i++) {
+            var waveHeightRange = Math.round(data.ForecastData[i].MinimumBreakingHeight * 100) / 100 + " - " + Math.round(data.ForecastData[i].MaximumBreakingHeight * 100) / 100 + " ft";
+            var swell = Math.round(data.ForecastData[i].SignificantWaveHeight * 100) / 100 + " @ " + Math.round(data.ForecastData[i].MeanWavePeriod * 100) / 100 + " s " + Math.round(data.ForecastData[i].DominantWaveDirection * 100) / 100 + "\xB0";
+            var wind  = Math.round(data.ForecastData[i].SurfaceWindDirection * 100) / 100 + "\xB0 " + Math.round(data.ForecastData[i].SurfaceWindSpeed * 100) / 100 + " mph";
+
+            dataPoint = {
+                y: Math.round(data.ForecastData[i].MaximumBreakingHeight * 100) / 100,
+                x: i,
+                label: data.ForecastData[i].Date.split(" ")[0] + " " + data.ForecastData[i].Time,
+                toolTipContent: "<b>{label}: " + waveHeightRange + "</b><br>Swell: " + swell + "<br>Wind: " + wind
+            }
+            dataSeries.push(dataPoint);
+        }
+
+        var chart = new CanvasJS.Chart('waveHeightChart', {
+            title: {
+                text: "Wave Height (ft)",
+                verticalAlign: "top", 
+                horizontalAlign: "center",
+                fontSize: 15,
+                margin: -15,
+                fontWeight: "bolder"
+            },
+            axisY: {
+                gridColor: "white",
+                lineColor: "#F2F2F2",
+                tickColor: "#F2F2F2",
+                gridThickness: 0           
+            },
+            axisX: {
+                lineColor: "#F2F2F2",
+                tickColor: "#F2F2F2",
+                labelAutoFit: true,
+                labelFontSize: 11
+            },
+            data: [
+            {        
+                type: "spline", 
+                dataPoints: dataSeries,
+                color: "#0D9EFF"
+            }       
+            ], 
+        });
+
+        chart.render();
+    });
 });
