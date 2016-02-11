ab6925e9c74d7ec21d3ecdf5c73c326c3084ba84
Matthew Iannucci <miannucci@navatekltd.com>
2016-02-11 13:23:46 -0500

    Start playing with charting

--- static/js/rhodycast.js
+++ static/js/rhodycast.js
@@ -1,7 +1,7 @@
 function initMap() {
     var latitude = parseFloat($('#map').attr('data-lat'));
     var longitude = parseFloat($('#map').attr('data-lng'));
-    
+
     map = new google.maps.Map($('#map')[0], {
         center: {lat: latitude, lng: longitude},
         zoom: 10,
@@ -16,5 +16,33 @@ function initMap() {
 }
 
 $(document).ready(function() {
-    // Setup stuff!
+    // Setup the chart!
+    var chart = new CanvasJS.Chart('waveHeightChart', {
+        title:{
+            text: "Wave Height (ft)"
+        },
+        axisY:{
+            gridColor: "white"
+        },
+        data: [
+        {        
+            type: "spline", dataPoints: [
+            { x: new Date(2012, 00, 1), y: 1352 },
+            { x: new Date(2012, 01, 1), y: 1514 },
+            { x: new Date(2012, 02, 1), y: 1321 },
+            { x: new Date(2012, 03, 1), y: 1163 },
+            { x: new Date(2012, 04, 1), y: 950 },
+            { x: new Date(2012, 05, 1), y: 1201 },
+            { x: new Date(2012, 06, 1), y: 1186 },
+            { x: new Date(2012, 07, 1), y: 1281 },
+            { x: new Date(2012, 08, 1), y: 1438 },
+            { x: new Date(2012, 09, 1), y: 1305 },
+            { x: new Date(2012, 10, 1), y: 1480 },
+            { x: new Date(2012, 11, 1), y: 1291 }        
+            ]
+        }       
+        ]
+    });
+
+    chart.render();
 });
