67fd4fcac98870be0eec1e33a289a847dbd5371d
miannucci <miannucci@navatekltd.com>
2016-02-12 14:22:09 -0500

    Remove maps references, add link to location in google maps

--- static/js/rhodycast.js
+++ static/js/rhodycast.js
@@ -1,20 +1,3 @@
-function initMap() {
-    var latitude = parseFloat($('#map').attr('data-lat'));
-    var longitude = parseFloat($('#map').attr('data-lng'));
-
-    map = new google.maps.Map($('#map')[0], {
-        center: {lat: latitude, lng: longitude},
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    });
-
-    var spotMarker = new google.maps.Marker({
-        position: {lat: latitude, lng: longitude},
-        map: map,
-        title: $('#map').attr('data-name')
-    });
-}
-
 $(document).ready(function() {
     // Setup the chart!
     $.ajax({
